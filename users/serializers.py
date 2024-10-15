a888bc37235ea9d111b22b24918cb676f377edd4
Michgarcia1 <sarahiggutierrez422@gmail.com>
2024-10-15 12:20:10 -0600

    archivo serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,4 @@
-from rest_framework import serializers, status
-from rest_framework.response import Response
+from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
